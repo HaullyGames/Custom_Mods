de1f486a56f83e5ac0ed6b63c957d6c7c04fb6ce
Haully Games <34033348+HaullyGames@users.noreply.github.com>
2018-10-05 15:08:23 -0400

    Initial

---
+++ Sync_MyChanges.ps1
@@ -0,0 +1,21 @@
+﻿$foldersCFG = @(`
+    "PlayYourWay" `
+    ,"SETIprobeParts" `
+    ,"RSSVE" `
+    )
+
+$foldersALL = @(`
+	"KSP_FOV" `
+    )
+
+foreach($folder in $foldersCFG)
+{
+    $path = [System.Environment]::GetEnvironmentVariable("KSPGAMEDATA","Machine") + "\" + $folder
+	robocopy $path .\$folder *.cfg /MIR /FFT /Z /XA:H /W:5
+}
+
+foreach($folder in $foldersALL)
+{
+    $path = [System.Environment]::GetEnvironmentVariable("KSPGAMEDATA","Machine") + "\" + $folder
+	robocopy $path .\$folder /MIR /FFT /Z /XA:H /W:5
+}
